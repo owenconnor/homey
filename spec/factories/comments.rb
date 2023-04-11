4fcf9db391fbc715d28d077ce52108ca4918acc1
Owen Connor <owenconnor@protonmail.com>
2023-04-11 11:42:19 +0100

    specs for comments

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -1,7 +1,12 @@
 FactoryBot.define do
   factory :comment do
-    body { "MyText" }
-    user { nil }
-    commentable { nil }
+    body { Faker::Lorem.paragraph }
+    user { FactoryBot.create(:user) }
+    commentable { FactoryBot.create(:project) }
+    trait :with_news_item do
+      after(:create) do |comment|
+        FactoryBot.create(:news_item, user: comment.user, title: "New comment on #{comment.commentable_type} #{comment.commentable_id}", body: comment.body, news_itemable: comment.commentable, project: comment.commentable)
+      end
+    end
   end
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,64 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "should have a valid factory" do
+      expect(FactoryBot.build(:comment)).to be_valid
+    end
+    it "should require a body" do
+      expect(FactoryBot.build(:comment, body: nil)).not_to be_valid
+    end
+    it "should require a body of at least 10 characters" do
+      expect(FactoryBot.build(:comment, body: "123456789")).not_to be_valid
+    end
+    it "should require a user_id" do
+      expect(FactoryBot.build(:comment, user_id: nil)).not_to be_valid
+    end
+    it "should require a user_id of an integer" do
+      expect(FactoryBot.build(:comment, user_id: "abc")).not_to be_valid
+    end
+    it "should require a commentable_id" do
+      expect(FactoryBot.build(:comment, commentable_id: nil)).not_to be_valid
+    end
+    it "should require a commentable_id of an integer" do
+      expect(FactoryBot.build(:comment, commentable_id: "abc")).not_to be_valid
+    end
+    it "should require a commentable_type" do
+      expect(FactoryBot.build(:comment, commentable_type: nil)).not_to be_valid
+    end
+    # it "should require a commentable_type of 'Project'" do
+    #   expect(FactoryBot.build(:comment, commentable_type: "abc")).not_to be_valid
+    # end
+  end
+
+  describe "associations" do
+    it "should belong to a user" do
+      expect(FactoryBot.build(:comment)).to respond_to(:user)
+    end
+    it "should belong to a commentable" do
+      expect(FactoryBot.build(:comment)).to respond_to(:commentable)
+    end
+    it "should have many news items" do
+      expect(FactoryBot.build(:comment)).to respond_to(:news_items)
+    end
+  end
+
+  describe "methods" do
+    describe "create_news_item" do
+      let!(:comment) { FactoryBot.create(:comment, :with_news_item) }
+      it "should create a news item" do
+        expect(Comment.last.news_items.count).to eq(1)
+      end
+    end
+
+  end
+
+  describe "callbacks" do
+    describe "after_create" do
+      let!(:comment) { FactoryBot.create(:comment) }
+      it "should create a news item" do
+        expect(Comment.last.news_items.count).to eq(1)
+      end
+    end
+  end
 end
