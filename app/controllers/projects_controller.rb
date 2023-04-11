b301d9760fba02887c1b2f769ef959b6889890ae
Owen Connor <owenconnor@protonmail.com>
2023-04-11 11:33:28 +0100

    added project model and controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,13 +2,12 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    Rails.logger.info "Current user: #{current_user.inspect}"
     @projects = Project.all
   end
 
   def show
     @project = Project.find(params[:id])
-    @comments = @project.comments
+    @news_items = NewsItem.where(project_id: @project.id)
   end
 
   def new
@@ -30,8 +29,22 @@ class ProjectsController < ApplicationController
     redirect_to projects_url, :notice => "Successfully destroyed project."
   end
 
+  def edit
+    @project = Project.find(params[:id])
+  end
+
+  def update
+    @project = Project.find(params[:id])
+    if @project.update(project_params)
+      @project.create_news_item(current_user)
+      redirect_to @project, :notice  => "Successfully updated project."
+    else
+      render :action => 'edit'
+    end
+  end
+
   private
   def project_params
-    params.require(:project).permit(:name, :description)
+    params.require(:project).permit(:name, :description, :status)
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,22 @@
 class Project < ApplicationRecord
   has_many :comments, as: :commentable
+  has_many :news_items, as: :news_itemable
 
+  VALID_STATUSES = ["pending","new", "active", "completed", "archived"]
+
+  validates :name, presence: true, length: { minimum: 4 }
+  validates :status, presence: true, inclusion: { in: VALID_STATUSES }
+  validates :description, presence: true, length: { minimum: 10 }
+
+  def create_news_item(user)
+    if self.saved_change_to_status?
+      ni = NewsItem.create(title: "#{self.name} project status update", body: "#{self.name} status changed to #{self.status}", news_itemable_type: "Project", news_itemable_id: id, project_id: self.id, user_id: user.id)
+      if ni.persisted?
+        Rails.logger.info "News item created: #{ni.inspect}"
+      else
+        Rails.logger.info "News item not created: #{ni.errors.full_messages}"
+      end
+    end
+  end
 
 end
