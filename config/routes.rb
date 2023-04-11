97ef0c37d0aa26c89e9e74e4c86bedc6bb10facf
Owen Connor <owenconnor@protonmail.com>
2023-04-11 11:41:25 +0100

    defined routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :users, only: [:show]
+  resources :projects do
+    resources :comments, only: [:create, :destroy]
+  end
+  resources :comments
 end
