b2ea05b4372f62239563c93c7bd75f474935c70d
Owen Connor <owenconnor@protonmail.com>
2023-04-11 11:32:40 +0100

    added comments model and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,33 @@
 class CommentsController < ApplicationController
+  before_action :set_project, only: [:create, :destroy]
+
+  def create
+    @comment = @project.comments.build(comment_params)
+    @comment.user = current_user
+
+    if @comment.save
+      flash[:notice] = "Comment was successfully created."
+      redirect_to @project, notice: "Comment was successfully created."
+    else
+      flash[:alert] = "Error creating comment. #{@comment.errors.full_messages.to_sentence}"
+      redirect_to @project, alert: "Error creating comment. #{@comment.errors.full_messages.to_sentence}"
+    end
+  end
+
+  def destroy
+    @comment = @project.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to @project, notice: "Comment was successfully deleted."
+  end
+
+  private
+
+  def set_project
+    @project = Project.find(params[:project_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,16 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
+  has_many :news_items, as: :news_itemable
+
+  after_create :create_news_item
+
+  validates :body, presence: true, length: { minimum: 10 }
+  validates :user_id, presence: true, numericality: { only_integer: true }
+  validates :commentable_id, presence: true, numericality: { only_integer: true }
+  validates :commentable_type, presence: true, inclusion: { in: ["Project"] }
+
+  def create_news_item
+    NewsItem.create(title: "New comment on #{self.commentable_type} #{self.commentable_id}", body: self.body, news_itemable_type: "Comment", news_itemable_id: id, project_id: self.commentable_id, user_id: self.user_id)
+  end
 end
