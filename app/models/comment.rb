b497730beb4ba2f2692d8a689defd04f6f9cabff
Owen Connor <owenconnor@protonmail.com>
2023-04-11 13:09:25 +0100

    comment name fix

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -11,6 +11,6 @@ class Comment < ApplicationRecord
   validates :commentable_type, presence: true, inclusion: { in: ["Project"] }
 
   def create_news_item
-    NewsItem.create(title: "New comment on #{self.commentable_type} #{self.commentable_id}", body: self.body, news_itemable_type: "Comment", news_itemable_id: id, project_id: self.commentable_id, user_id: self.user_id)
+    NewsItem.create(title: "New comment on #{self.commentable.name}", body: self.body, news_itemable_type: "Comment", news_itemable_id: id, project_id: self.commentable_id, user_id: self.user_id)
   end
 end
